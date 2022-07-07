0102e5ce3bcc3704f877cfdd36f6de80cb4aec5c
JonathanHerrera09 <ttherrera30@gmail.com>
2022-07-07 18:50:34 -0500

    probando los nuevos plugins

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import './App.css';
 import React from "react";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 /* import Home from './Pages/Home/Home' */
-/* import Empresas from './Pages/Empresas/Empresas' */
+import Empresas from './Pages/Empresas/Empresas'
 import NavBar from './components/NavBar/NavBar'
-/* import Categorias from './Pages/Categorias/Categorias' */
+import Categorias from './Pages/Categorias/Categorias'
 import Footer from './components/Footer/Footer';
 import Nosotros from './Pages/Nosotros/Nosotros';
-/* import Productos from './Pages/Productos/Productos'; */
+import Productos from './Pages/Productos/Productos';
 import Contactenos from "./Pages/Contactenos/Contactenos";
 
 function App() {
@@ -18,11 +18,11 @@ function App() {
    <BrowserRouter>
    <NavBar/>
    <Routes>
-        {/* <Route path='/' element = {<Home/>} />
+        {/* <Route path='/' element = {<Home/>} />*/}
         <Route path='/Empresas/:name' element = {<Empresas/>} />
-        <Route path='/categorias' element = {<Categorias/>} /> */}
+        <Route path='/categorias' element = {<Categorias/>} /> 
         <Route path='/somos' element = {<Nosotros/>} />
-        {/* <Route path='/productos/:empresa' element = {<Productos/>} /> */}
+        <Route path='/productos/:empresa' element = {<Productos/>} />
         <Route path='/contacto' element = {<Contactenos/>} />
    </Routes>
    </BrowserRouter>
