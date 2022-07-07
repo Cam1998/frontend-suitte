f288bd1d8d0a0e02a0618a6acabbd037278025d1
JonathanHerrera09 <ttherrera30@gmail.com>
2022-07-07 16:47:31 -0500

    otra cosa

--- src/App.js
+++ src/App.js
@@ -1,28 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
-import React from "react";
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Nosotros from './Pages/Nosotros/Nosotros';
 
-const App = () =>{
+function App() {
   return (
-    <>
-    <BrowserRouter>
-    {/* <NavBar/> */}
-     <Routes>
-       {/* <Route path='/' element = {<Home/>} />
-       <Route path='/Empresas/:name' element = {<Empresas/>} />
-       <Route path='/categorias' element = {<Categorias/>} /> */}
-       <Route path='/somos' element = {<Nosotros/>} />
-{/*        <Route path='/productos/:empresa' element = {<Productos/>} />
-       <Route path='/contacto' element = {<Contactenos/>} /> */}
-     </Routes>
-     </BrowserRouter>
-     <br></br>
-     <br></br>
-     <br></br>
-     {/* <Footer/> */}
-   </>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Suitte 2.0
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
