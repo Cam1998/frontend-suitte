592d0667744fb38ae33e7bbbb687204463782e4b
JonathanHerrera09 <ttherrera30@gmail.com>
2022-07-07 17:15:34 -0500

    probando los nuevos plugins

--- src/App.js
+++ src/App.js
@@ -2,17 +2,27 @@ import logo from './logo.svg';
 import './App.css';
 import React from "react";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     
     <>
    <BrowserRouter>
-   
+   {/* <NavBar/> */}
    <Routes>
-     
+        {/* <Route path='/' element = {<Home/>} />
+        <Route path='/Empresas/:name' element = {<Empresas/>} />
+        <Route path='/categorias' element = {<Categorias/>} />
+        <Route path='/somos' element = {<Nosotros/>} />
+        <Route path='/productos/:empresa' element = {<Productos/>} />
+        <Route path='/contacto' element = {<Contactenos/>} /> */}
    </Routes>
    </BrowserRouter>
+      <br></br>
+      <br></br>
+      <br></br>
+    <Footer/>
     </>
   );
 }
