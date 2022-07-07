0e7c01aaa4ad6cf4f9d8b8421cd93adf83d7977b
JonathanHerrera09 <ttherrera30@gmail.com>
2022-07-07 17:02:49 -0500

    probando los nuevos plugins

--- src/App.js
+++ src/App.js
@@ -5,22 +5,15 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Suitte 2.0
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+    <>
+   <BrowserRouter>
+   
+   <Routes>
+     
+   </Routes>
+   </BrowserRouter>
+    </>
   );
 }
 
